d624e702d3c0ce9f8466e6287329c08490ab7381
repl.it user <replituser@example.com>
2020-10-19 20:57:19 +0000

    new try

--- heap.c
+++ heap.c
@@ -88,10 +88,12 @@ void heap_push(Heap* pq, void* data, int priority)
   }
   pq->heapArray[index].data =(data);
   pq->heapArray[index].priority =(priority);
+  printf("%d \n",pq->heapArray[index].priority);
   if (index!=0)
   {
     switch_Node(pq->heapArray,index);
   }
+  printf("%d \n",pq->heapArray[index].priority);
   pq->size =((pq->size)+1);
 }
 
